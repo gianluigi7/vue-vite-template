018fdb8a6caa0d0d6c0720e38b5b0d7166526722
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-06-06 19:03:24 +0200

    template H/M/F

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+ export default {
+    name: 'AppHeader',
+ }
+</script>
+
+<template>
+ <h1>template vue/vite</h1>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+ export default {
+    name: 'AppMain',
+ }
+</script>
+
+<template>
+ <main>
+    main
+ </main>
+</template>
+
+<style scoped lang="scss">
+
+</style>
